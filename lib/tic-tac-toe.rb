7eda691881d275c821822c28593d6df37ff6ed21
nax2uk <azlinayeo@gmail.com>
2020-09-23 12:31:56 +0100

    printed out welcome message onto the console

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -1,8 +1,9 @@
 class TicTacToe
     def output_welcome_message
-        puts "Hello"
+        puts "Welcome to Tic Tac Toe!"
     end
 end
+
 
 tic_tac_toe = TicTacToe.new
 tic_tac_toe.output_welcome_message
