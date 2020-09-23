63e9dbabdf9418d871cdc8f647a48facebcc6efd
nax2uk <azlinayeo@gmail.com>
2020-09-23 12:15:02 +0100

    added first acceptance test

---
+++ spec/acceptance_tests/test_1_spec.rb
@@ -0,0 +1,16 @@
+# Test 1: It will display a start game message and instructions.
+# Given: The user starts a game.
+# When: When the user wants to start the game on the command line
+# Then: A welcome and instructions will appear.
+
+describe "user starts a game" do
+    context "at the start of the game" do
+        xit "displays a welcome message" do
+            # a new game is initialised, displays a welcome message
+        end
+
+        xit "displays instructions" do
+            # displays instructions on a new line after welcome message
+        end
+    end
+end
