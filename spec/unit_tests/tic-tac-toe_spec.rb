6972a13e3a84836063571dddd06a5f6e1d1f1ff8
nax2uk <azlinayeo@gmail.com>
2020-09-23 12:33:10 +0100

    refactored test to check for welcome message on the screen

--- spec/unit_tests/tic-tac-toe_spec.rb
+++ spec/unit_tests/tic-tac-toe_spec.rb
@@ -2,8 +2,20 @@ require "./lib/tic-tac-toe"
 
 describe TicTacToe do
     context "when a new game started" do
-        it "outputs hello as a welcome message" do
-            expect(TicTacToe.new.output_welcome_message).to eq("Hello")
+        it "outputs a welcome message" do
+            expect do
+                TicTacToe.new.output_welcome_message
+            end.to output("Welcome to Tic Tac Toe!\n").to_stdout
         end
     end
 end
+
+# describe MakeIt do
+#     describe '#awesome' do
+#       it 'prints awesome things' do
+#         expect do
+#           MakeIt.new.awesome('tests')
+#         end.to output('Awesome tests').to_stdout
+#       end
+
+#       expect { something }.to output("hello\n").to_stdout
