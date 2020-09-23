09824333b0ead6b633e913f1d5b4881f740c0e5b
nax2uk <azlinayeo@gmail.com>
2020-09-23 14:52:59 +0100

    added display instructions test and passed it

---
+++ lib/main.rb
@@ -0,0 +1,5 @@
+require_relative 'tic_tac_toe'
+
+tic_tac_toe = TicTacToe.new
+tic_tac_toe.output_welcome_message
+tic_tac_toe.output_instructions
---
+++ spec/unit_tests/tic_tac_toe_spec.rb
@@ -0,0 +1,16 @@
+require "./lib/tic_tac_toe"
+
+describe TicTacToe do
+    context "when a new game started" do
+        it "outputs a welcome message" do
+            expect do
+                TicTacToe.new.output_welcome_message
+            end.to output("Welcome to Tic Tac Toe!\n").to_stdout
+        end
+        it "outputs instructions for game after welcome message" do
+            expect do
+                TicTacToe.new.output_instructions
+            end.to output("Instructions:\nThe game is played on a 3x3 grid.\nYou are X, your opponent is O. Players take turns putting their marks in empty squares.\nThe first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.\nIf all 9 squares are full and no player has 3 marks in a row, the game is over.\n").to_stdout
+        end
+    end
+end
