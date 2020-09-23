1e916c5b73674e861bc694bd2cd08db59ca8efcf
nax2uk <azlinayeo@gmail.com>
2020-09-23 15:23:19 +0100

    written acceptance test 2

--- spec/acceptance_tests/test_1_spec.rb
+++ spec/acceptance_tests/test_1_spec.rb
@@ -3,14 +3,21 @@
 # When: When the user wants to start the game on the command line
 # Then: A welcome and instructions will appear.
 
-describe "user starts a game" do
+require './lib/tic_tac_toe'
+
+describe "AC Test 1: User starts a game" do
     context "at the start of the game" do
-        xit "displays a welcome message" do
-            # a new game is initialised, displays a welcome message
+        tic_tac_toe = TicTacToe.new
+        it "displays a welcome message" do
+            expect do
+                tic_tac_toe.output_welcome_message
+            end.to output("Welcome to Tic Tac Toe!\n").to_stdout
         end
 
-        xit "displays instructions" do
-            # displays instructions on a new line after welcome message
+        it "displays instructions" do
+            expect do
+                tic_tac_toe.output_instructions
+            end.to output("Instructions:\nThe game is played on a 3x3 grid.\nYou are X, your opponent is O. Players take turns putting their marks in empty squares.\nThe first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.\nIf all 9 squares are full and no player has 3 marks in a row, the game is over.\n").to_stdout
         end
     end
 end
--- spec/unit_tests/tic_tac_toe_spec.rb
+++ spec/unit_tests/tic_tac_toe_spec.rb
@@ -13,4 +13,10 @@ describe TicTacToe do
             end.to output("Instructions:\nThe game is played on a 3x3 grid.\nYou are X, your opponent is O. Players take turns putting their marks in empty squares.\nThe first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.\nIf all 9 squares are full and no player has 3 marks in a row, the game is over.\n").to_stdout
         end
     end
+
+    # context "after user has read the instructions" do
+    #     it "can take a user input" do
+       
+    #     end
+    # end
 end
