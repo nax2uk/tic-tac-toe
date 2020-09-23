ae6f623315e2917b0c862ce17866f4d4669f934d
nax2uk <azlinayeo@gmail.com>
2020-09-23 16:16:46 +0100

    added test2

---
+++ spec/acceptance_tests/test_2_spec.rb
@@ -0,0 +1,11 @@
+# Test 2: It will output an empty board at the start of the game.
+# Given: A user has started the game and read the instructions
+# When: the user the inputs "y" to continue.
+# Then: an empty board will appear.
+
+describe "AC Test 2: User has read the instructions and wants to continue with the game" do
+    context "After user inputs 'y' to continue" do
+        it "displays an empty board" do
+        end
+    end
+end
