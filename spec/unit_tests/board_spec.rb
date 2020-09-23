6474b738ee7295795a2fe0bd2cd9e31344356117
nax2uk <azlinayeo@gmail.com>
2020-09-23 16:15:46 +0100

    created test for Board

---
+++ spec/unit_tests/board_spec.rb
@@ -0,0 +1,26 @@
+require "./lib/board"
+
+describe Board do
+    context "displays empty board" do
+        it "displays number coordinates" do
+            expect do
+                Board.new.prints_number_coord
+            end.to output("  1   2   3    \n").to_stdout
+        end
+        it "displays one horizontal line of the board" do
+            expect do
+                Board.new.prints_horizontal_board_line
+            end.to output("+---+---+---+\n").to_stdout
+        end
+
+
+# "     1   2   3  "
+# "   +---+---+---+ "
+# "A  | X | O | X | "
+# "   +---+---+---+ "
+# "B  |   |   |   |"
+# "   +---+---+---+ "
+# "C  |   |   |   |"
+# "   +---+---+---+ "
+    end
+end
