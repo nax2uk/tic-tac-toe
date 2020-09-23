9b82e1e24003cb03b92d89639eab944616df2a08
nax2uk <azlinayeo@gmail.com>
2020-09-23 16:15:15 +0100

    created Board class

---
+++ lib/board.rb
@@ -0,0 +1,38 @@
+class Board
+
+    def prints_horizontal_board_line
+        puts "+---+---+---+"
+    end
+    # def prints_number_coord
+    #     puts "    1   2    3"
+    # end
+    # def print_1st_row 
+    #     puts "A|   |   |   |   |"
+    # end
+    # def print_2nd_row 
+    #     puts "B|   |   |   |   |"
+    # end
+    # def print_3rd_row 
+    #     puts "C|   |   |   |   |"
+    # end
+    # def prints_empty_board
+    #     self.prints_number_coord
+    #     self.prints_horizontal_board_line
+    #     self.print_row("A")
+    #     self.prints_horizontal_board_line
+    #     self.print_row("B")
+    #     self.prints_horizontal_board_line
+    #     self.print_row("C")
+    #     self.prints_horizontal_board_line
+    # end
+    # def print_current_board
+    #     self.prints_number_coord
+    #     self.prints_horizontal_board_line
+    #   #  self.print_row("A")
+    #     self.prints_horizontal_board_line
+    #   #  self.print_row("B")
+    #     self.prints_horizontal_board_line
+    #   #  self.print_row("C")
+    #     self.prints_horizontal_board_line
+    # end
+end
