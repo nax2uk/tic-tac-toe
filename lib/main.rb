48702c5279f60e272be852733ea8adad8bb7d01a
Chloe <chloe.wong@madetech.com>
2020-09-24 15:14:30 +0100

    multiline instructions now tidied using HEREDOC

--- lib/main.rb
+++ lib/main.rb
@@ -5,5 +5,5 @@ tic_tac_toe = TicTacToe.new
 tic_tac_toe.output_welcome_message
 tic_tac_toe.output_instructions
 
-board = Board.new
-board.prints_empty_board
+# board = Board.new
+# board.prints_empty_board
--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,16 @@ class TicTacToe
     end
 
     def output_instructions
-        puts "Instructions:\n\nThe game is played on a 3x3 grid.\nYou are X, your opponent is O. Players take turns putting their marks in empty squares.\nThe first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.\nIf all 9 squares are full and no player has 3 marks in a row, the game is over.\n\n\n"
+        puts <<~HEREDOC
+            Instructions:
+
+            The game is played on a 3x3 grid.
+            You are X, your opponent is O. Players take turns putting their marks in empty squares.
+            The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
+            If all 9 squares are full and no player has 3 marks in a row, the game is over.
+
+
+        HEREDOC
     end
 
     # def set_continue
