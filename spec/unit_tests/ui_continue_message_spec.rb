1a6961eca31f7f265c6b90afaa81f828d06992e6
Chloe <chloe.wong@madetech.com>
2020-09-28 15:22:31 +0100

    added function name to tests for easier debugging

--- spec/unit_tests/ui_continue_message_spec.rb
+++ spec/unit_tests/ui_continue_message_spec.rb
@@ -1,19 +1,28 @@
 require './lib/UI/ui_continue_message'
 describe UIContinueMessage do
     let(:ui_continue_message) {UIContinueMessage.new}
-    context "after instructions have been printed to the terminal" do
-        it "prints 'do you want to continue y/n'" do
-            expect do
+    context "#capture_input" do
+        context "after instructions have been printed to the terminal" do
+            it "prints 'do you want to continue y/n'" do
+                expect do
+                    allow(ui_continue_message).to receive(:gets).and_return("y\n")
+                    ui_continue_message.capture_input
+                end.to output("Do you want to continue? y/n: ").to_stdout
+            end
+        end
+
+        context "after asking 'do you want to continue?'" do
+            it "accepts an input" do
                 allow(ui_continue_message).to receive(:gets).and_return("y\n")
-                ui_continue_message.capture_input
-            end.to output("Do you want to continue? y/n: ").to_stdout
+                expect(ui_continue_message.capture_input).to eq("y")
+            end
         end
     end
 
-    context "after asking 'do you want to continue?'" do
-        it "accepts an input" do
-            allow(ui_continue_message).to receive(:gets).and_return("y\n")
-            expect(ui_continue_message.capture_input).to eq("y")
+    context "#validate_input" do
+        context "" do
+            it "" do
+            end
         end
     end
 
--- spec/unit_tests/ui_standard_message_spec.rb
+++ spec/unit_tests/ui_standard_message_spec.rb
@@ -3,35 +3,40 @@ require "./lib/UI/ui_standard_message"
 describe UIStandardMessage do
     let(:ui_standard_message) {UIStandardMessage.new}
     context "when a new game started" do
-        it "outputs a welcome message" do
-            expect do
-                ui_standard_message.output_welcome_message
-            end.to output("Welcome to Tic Tac Toe!\n\n").to_stdout
+
+        context "#output_welcome_message" do
+            it "outputs a welcome message" do
+                expect do
+                    ui_standard_message.output_welcome_message
+                end.to output("Welcome to Tic Tac Toe!\n\n").to_stdout
+            end
         end
 
-        it "outputs instructions for game after welcome message" do
-            expect do
-                ui_standard_message.output_instructions
-            end.to output(
-                <<~HEREDOC
-                Instructions:
-                
-                The game is played on a 3x3 grid.
-                You are X, your opponent is O. Players take turns putting their marks in empty squares.
-                The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
-                If all 9 squares are full and no player has 3 marks in a row, the game is over.
-                
-                
-                HEREDOC
-                ).to_stdout
+        context "#output_instruction" do
+            it "outputs instructions for game after welcome message" do
+                expect do
+                    ui_standard_message.output_instructions
+                end.to output(
+                    <<~HEREDOC
+                    Instructions:
+                    
+                    The game is played on a 3x3 grid.
+                    You are X, your opponent is O. Players take turns putting their marks in empty squares.
+                    The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
+                    If all 9 squares are full and no player has 3 marks in a row, the game is over.
+                    
+                    
+                    HEREDOC
+                    ).to_stdout
+            end
         end
     end
+
 
     context "after user has read the instructions" do
         xit "can take a user input" do
        
         end
     end
-
 
 end
