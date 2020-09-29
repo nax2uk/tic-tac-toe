7c83f61e474d8b32c0a53146db1daf522fd5e68f
nax2uk <azlinayeo@gmail.com>
2020-09-29 11:07:18 +0100

    passes test to take in 'A' and 'B' as row coordinates

--- lib/Board/board.rb
+++ lib/Board/board.rb
@@ -2,10 +2,15 @@ class Board
     attr_accessor :board_array
 
     def initialize 
-        @board_array = Array.new(3) {Array.new(3, "")}
+        @board_array = Array.new(3) {Array.new(3, "")} 
     end
 
     def input_entry(symbol, row, column)
-        [["X","",""], ["","",""], ["","",""]]
+        if row == "A"
+            @board_array[0][column.to_i - 1] = symbol
+        end
+        if row == "B"
+            @board_array[1][column.to_i - 1] = symbol
+        end
     end
 end
--- spec/unit_tests/Board/board_spec.rb
+++ spec/unit_tests/Board/board_spec.rb
@@ -9,8 +9,22 @@ describe Board do
         end
     end
     context "#input_entry" do
-        it "takes in an entry and adds it into the board array" do
-            expect(board.input_entry("X", "A", "1")).to eq([["X","",""], ["","",""], ["","",""]])
+        it "first entry - takes in an entry and adds it into the board array" do
+            board.input_entry("X", "A", "1")
+            expect(board.board_array).to eq([["X","",""], ["","",""], ["","",""]])
+            # expect(board.input_entry("X", "A", "2")).to eq([["","X",""], ["","",""], ["","",""]])
+            # expect(board.input_entry("X", "A", "3")).to eq([["","","X"], ["","",""], ["","",""]])
+
+        end
+        it "takes in 2 entries consecutively and adds them to the board array" do
+            board.input_entry("X", "A", "1")
+            board.input_entry("X", "A", "2")
+            expect(board.board_array).to eq([["X","X",""], ["","",""], ["","",""]])
+        end
+
+        it "takes in 1 entry and adds it to location (B, 1) in the board array" do
+            board.input_entry("X", "B", "1")
+            expect(board.board_array).to eq([["","",""], ["X","",""], ["","",""]])
         end
     end
 end
