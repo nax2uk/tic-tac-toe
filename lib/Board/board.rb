69d6bc93ea42c04820166786ec9d0baf9d042201
nax2uk <azlinayeo@gmail.com>
2020-09-29 10:11:37 +0100

    board array test to take in an input passes

--- lib/Board/board.rb
+++ lib/Board/board.rb
@@ -1,7 +1,11 @@
 class Board 
     attr_accessor :board_array
+
     def initialize 
-        @board_array = [["","",""], ["","",""], ["","",""]]
+        @board_array = Array.new(3) {Array.new(3, "")}
+    end
+
+    def input_entry(symbol, row, column)
+        [["X","",""], ["","",""], ["","",""]]
     end
 end
-
--- spec/unit_tests/Board/board_spec.rb
+++ spec/unit_tests/Board/board_spec.rb
@@ -1,10 +1,16 @@
 require './lib/Board/board'
 
 describe Board do
+    let(:board) {Board.new}
+
     context "#initialize" do
         it "creates a new board that is a 3x3 array" do
-            board = Board.new
             expect(board.board_array).to eq([["","",""], ["","",""], ["","",""]])
+        end
+    end
+    context "#input_entry" do
+        it "takes in an entry and adds it into the board array" do
+            expect(board.input_entry("X", "A", "1")).to eq([["X","",""], ["","",""], ["","",""]])
         end
     end
 end
