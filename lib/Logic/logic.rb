b97e0f2ae63ab588286b9de002612f8728376e86
Chloe <chloe.wong@madetech.com>
2020-10-05 16:31:20 +0100

    test for same_entries_in_a_line is completed

--- lib/Logic/logic.rb
+++ lib/Logic/logic.rb
@@ -1,7 +1,22 @@
 class Logic
-    def has_won?(array)
-        false
-        #array = [["X","X","X"], ["","",""], ["","",""]]
+
+    # will be called after every time a user / computer inputs an entry
+    def has_won?(row, column, board)
+        # row_array = @board.get_row(row)
+        # column_array = @board.get_column(column)
+        # left_diagonal_array = @board.get_left_diagonal
+        # right_diagonal_array = @board.get_right_diagonal
+
+        # self.same_entries_in_a_line(row_array) || 
+        # self.same_entries_in_a_line(column_array) ||
+        # self.same_entries_in_a_line(left_diagonal_array) ||
+        # self.same_entries_in_a_line(right_diagonal_array)
     end
+
+    # checks if same entry in a line
+    def same_entries_in_a_line(line_array)
+        line_array.uniq.size <= 1 && line_array[0] != ""
+    end 
+
 end
 
--- spec/unit_tests/Logic/logic_spec.rb
+++ spec/unit_tests/Logic/logic_spec.rb
@@ -1,16 +1,79 @@
 require './lib/Logic/logic'
+require './lib/Board/board'
 
 describe Logic do
     let(:logic) {Logic.new}
-    context "#has_won?" do
-        it "returns false if there is only one 'X' in the board array" do
-            array = [["X","",""], ["","",""], ["","",""]]
-            expect(logic.has_won?(array)).to eq(false)
-        end
+    let(:board) {Board.new}
 
-        xit "returns true if the first row contains three 'X's" do
-            array = [["X","X","X"], ["","",""], ["","",""]]
-            expect(logic.has_won?(array)).to eq(true)
+    context "#same_entries_in_a_line" do
+        it "returns false if row entries are not the same" do
+            board.input_entry("X", "A", "1")
+            row_array = board.get_row("A")
+            expect(logic.same_entries_in_a_line(row_array)).to eq(false)
+        end
+        it "returns false if column entries are not the same" do
+            board.input_entry("X", "A", "1")
+            column_array = board.get_column("1")
+            expect(logic.same_entries_in_a_line(column_array)).to eq(false)
+        end
+        it "returns false if left diagonal entries are not the same" do
+            board.input_entry("X", "A", "1")
+            left_diagonal_array = board.get_left_diagonal
+            expect(logic.same_entries_in_a_line(left_diagonal_array)).to eq(false)
+        end
+        it "returns false if right diagonal entries are not the same" do
+            board.input_entry("X", "A", "1")
+            right_diagonal_array = board.get_right_diagonal
+            expect(logic.same_entries_in_a_line(right_diagonal_array)).to eq(false)
+        end
+        it "returns false if right diagonal entries are not the same" do
+            board.input_entry("X", "B", "1")
+            right_diagonal_array = board.get_right_diagonal
+            expect(logic.same_entries_in_a_line(right_diagonal_array)).to eq(false)
+        end
+        it "returns true if column entries are the same" do
+            board.input_entry("X", "A", "1")
+            board.input_entry("X", "B", "1")
+            board.input_entry("X", "C", "1")
+            column_array = board.get_column("1")
+            expect(logic.same_entries_in_a_line(column_array)).to eq(true)
+        end
+        it "returns true if row entries are the same" do
+            board.input_entry("X", "C", "1")
+            board.input_entry("X", "C", "2")
+            board.input_entry("X", "C", "3")
+            row_array = board.get_row("C")
+            expect(logic.same_entries_in_a_line(row_array)).to eq(true)
+        end
+        it "returns true if left diagonal entries are the same" do
+            board.input_entry("O", "A", "1")
+            board.input_entry("O", "B", "2")
+            board.input_entry("O", "C", "3")
+            left_diagonal_array = board.get_left_diagonal
+            expect(logic.same_entries_in_a_line(left_diagonal_array)).to eq(true)
+        end
+        it "returns true if right diagonal entries are the same" do
+            board.input_entry("O", "A", "3")
+            board.input_entry("O", "B", "2")
+            board.input_entry("O", "C", "1")
+            right_diagonal_array = board.get_right_diagonal
+            expect(logic.same_entries_in_a_line(right_diagonal_array)).to eq(true)
+        end
+        it "returns true if right diagonal entries are the same" do
+            board.input_entry("X", "A", "3")
+            board.input_entry("O", "A", "1")
+            board.input_entry("X", "B", "2")
+            board.input_entry("X", "C", "3")
+            board.input_entry("X", "C", "1")
+            right_diagonal_array = board.get_right_diagonal
+            expect(logic.same_entries_in_a_line(right_diagonal_array)).to eq(true)
+        end
+        
+    end
+
+    context "#has_won?" do
+        it "returns false if it's an empty board" do
+            
         end
     end
 end
