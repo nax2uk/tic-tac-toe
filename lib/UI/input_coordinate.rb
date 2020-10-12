403ff76c530bab94c5ac09e11a22d56ea74de085
nax2uk <azlinayeo@gmail.com>
2020-10-12 14:19:07 +0100

    input column coordinate tests pass

--- lib/UI/input_coordinate.rb
+++ lib/UI/input_coordinate.rb
@@ -11,5 +11,17 @@ module UI
             end
             false
         end
+
+        def input_column_coordinate
+            print "Please input column coordinate of 'O':"
+            gets.chomp
+        end
+
+        def validate_column_coordinate(column)
+            if column == "1" || column == "2" || column == "3"
+                return true
+            end
+            false
+        end
     end
 end
--- spec/unit_tests/UI/input_coordinate_spec.rb
+++ spec/unit_tests/UI/input_coordinate_spec.rb
@@ -28,4 +28,31 @@ describe UI::InputCoordinate do
             expect(ui_input_coordinate.validate_row_coordinate("A")).to eq(true)
         end
     end
+
+    context "#input_column_coordinate" do
+
+        context "after asking for column coordinate" do
+            it "takes a user input of 1" do
+                allow(ui_input_coordinate).to receive(:gets).and_return("1\n")
+                expect(ui_input_coordinate.input_column_coordinate).to eq("1")
+            end
+
+            it "takes a user input of 2" do
+                allow(ui_input_coordinate).to receive(:gets).and_return("2\n")
+                expect(ui_input_coordinate.input_column_coordinate).to eq("2")
+            end
+
+        end
+    end
+
+    context "#validate_column_coordinate" do
+        it "takes a user input that is not '1', '2' or '3' and returns false" do
+            #allow(ui_input_coordinate).to receive(:gets).and_return("D\n")
+            expect(ui_input_coordinate.validate_column_coordinate("4")).to eq(false)
+        end
+        it "takes a user input that is '1', returns true" do
+            #allow(ui_input_coordinate).to receive(:gets).and_return("D\n")
+            expect(ui_input_coordinate.validate_column_coordinate("1")).to eq(true)
+        end
+    end
 end
