5b0beff0833b6c1a1e90ba4ae32a53a85f933b91
nax2uk <azlinayeo@gmail.com>
2020-10-12 14:04:25 +0100

    moved functions to newly created Controller::UIController

--- main.rb
+++ main.rb
@@ -2,40 +2,13 @@
 # Each component can have their individual controllers ()
 # But can have one central controller which is what we have on lines 40/41
 
-require './lib/UI/continue_message'
-require './lib/UI/board'
-require './lib/Controller/start_game'
+require './lib/Controller/ui_controller'
 
-#  move to controller
-def print_board_or_exit_game(continue)
-    
-    if continue == 'y'
-        board = UI::Board.new
-        board.prints_empty_board
-    elsif continue == 'n'
-        exit(true)
-    end
-end
-# move to controller
-def print_continue_and_go_to_next_step
-    ui_continue_message = UI::ContinueMessage.new
-    continue = ''
-    continue_is_valid = false
+ui_controller = Controller::UIController.new
 
-    until continue_is_valid
-        continue = ui_continue_message.capture_input
-        continue_is_valid = ui_continue_message.validate_input(continue)
-    end
+ui_controller.print_welcome_and_output_instructions
 
-    # continue_is_valid == true
-    print_board_or_exit_game(continue)
-
-end
-
-# move to controller
-
-print_welcome_and_output_instructions
-print_continue_and_go_to_next_step
-
+continue = ui_controller.print_continue
+ui_controller.print_board_or_exit_game(continue)
 
 
