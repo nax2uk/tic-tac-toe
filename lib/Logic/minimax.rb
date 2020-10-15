72f35159e79144fed6926187878ae16f243d1448
nax2uk <azlinayeo@gmail.com>
2020-10-15 10:39:21 +0100

    combined arrange and act in one

--- lib/Logic/minimax.rb
+++ lib/Logic/minimax.rb
@@ -1,7 +1,27 @@
 module Logic
     class Minimax
-        def best_move
-            ['C','3']
+
+        ROW = {
+            'A'=> 0, 
+            'B'=> 1,
+            'C'=> 2
+        }.freeze
+
+        COL = {
+                '1'=> 0,
+                '2'=> 1,
+                '3'=> 2
+        }.freeze 
+
+        def best_move(board)
+            for row in 0..2
+                for col in 0..2
+                    if board.board_array[row][col] == " "
+              
+                        return [ROW.key(row), COL.key(col)]
+                    end
+                end
+            end
         end
         
     end
--- spec/unit_tests/Logic/minimax_spec.rb
+++ spec/unit_tests/Logic/minimax_spec.rb
@@ -6,13 +6,18 @@ describe Logic::Minimax do
     let(:minimax) {Logic::Minimax.new}
     context "#best_move" do
         it "returns ['C','3'] when there is only one empty space left" do
-            # Arrange
-              
-            # Act
-            allow(board).to receive(:board_array).and_return( [["X","O","X"], ["X","O","O"], ["O","X"," "]])
+            # Arrange && Act
+            allow(board).to receive(:board_array).and_return([["X","O","X"], ["X","O","O"], ["O","X"," "]])
             
             # Assert
-            expect(minimax.best_move).to eq(["C","3"])
+            expect(minimax.best_move(board)).to eq(["C","3"])
+        end
+        it "returns ['B','2'] when there is only one empty space left" do
+            # Arrange && Act
+            allow(board).to receive(:board_array).and_return( [["X","O","X"], ["O"," ","O"], ["X","O","X"]])
+            
+            # Assert
+            expect(minimax.best_move(board)).to eq(["B","2"])
         end
     end 
 
