e0368ff50048e5be0e4a714f1df6174d3d5d231b
Chloe <chloe.wong@madetech.com>
2020-10-22 14:30:54 +0100

    UI board edited for 5x5 board

--- lib/UI/board.rb
+++ lib/UI/board.rb
@@ -19,15 +19,15 @@ module UI
     # row[13]
 
         def print_number_coord_row
-            puts "     1   2   3  "
+            puts "     1   2   3   4   5"
         end
 
         def print_horizontal_board_line
-            puts "   +---+---+---+ \n"
+            puts "   +---+---+---+---+---+ \n"
         end
 
         def print_empty_row(row)
-            puts "#{row}  |   |   |   | \n"
+            puts "#{row}  |   |   |   |   |   | \n"
         end
 
         #Private methods - no one should care about them, including the tests
@@ -41,13 +41,19 @@ module UI
             self.print_horizontal_board_line
             self.print_empty_row("C")
             self.print_horizontal_board_line
+            self.print_empty_row("D")
+            self.print_horizontal_board_line
+            self.print_empty_row("E")
+            self.print_horizontal_board_line
         end
 
         def print_row(row, row_array)
-            row_string = "#{row}  |   |   |   | \n"
+            row_string = "#{row}  |   |   |   |   |   | \n"
             row_string[5] = row_array[0]
             row_string[9] = row_array[1]
             row_string[13] = row_array[2]
+            row_string[17] = row_array[3]
+            row_string[21] = row_array[4]
             puts row_string
         end
         def print_current_board(board)
@@ -58,6 +64,10 @@ module UI
             self.print_row("B", board[1])
             self.print_horizontal_board_line
             self.print_row("C", board[2])
+            self.print_horizontal_board_line
+            self.print_row("D", board[3])
+            self.print_horizontal_board_line
+            self.print_row("E", board[4])
             self.print_horizontal_board_line
         end
     end
