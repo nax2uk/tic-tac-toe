d96fb23c1d9fe287cb1860ee65f1c2d7576594c5
Chloe <chloe.wong@madetech.com>
2020-10-23 14:49:16 +0100

    added shebang to main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 # controller (model view controller) - takes input from the user and sends the data off to the back end
 # Each component can have their individual controllers ()
 # But can have one central controller which is what we have on lines 40/41
